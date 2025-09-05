18a96db98769872a66dcd2fd9007b616c77bb2c5
Coderyounes <younesanas2021@gmail.com>
2025-09-05 18:57:15 +0100

    add methods for task management in TaskRepository interface

--- src/main/java/com/taskguard/taskguard/domain/repository/TaskRepository.java
+++ src/main/java/com/taskguard/taskguard/domain/repository/TaskRepository.java
@@ -1,4 +1,14 @@
 package com.taskguard.taskguard.domain.repository;
 
+import com.taskguard.taskguard.domain.model.Task;
+
+import java.util.Optional;
+import java.util.List;
+
 public interface TaskRepository {
+    Task save(Task task);
+    Optional<Task> findById(Long id);
+    List<Task> findAll();
+    void deleteById(Long id);
+    void delete(Task task);
 }
