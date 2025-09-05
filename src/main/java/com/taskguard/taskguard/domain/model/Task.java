67e751dcac5b84ca7ea2c88cf5f8ad796db157f3
Coderyounes <younesanas2021@gmail.com>
2025-09-06 00:10:04 +0100

    add Task entity with fields for title, description, priority, status, due date, and timestamps

--- src/main/java/com/taskguard/taskguard/domain/model/Task.java
+++ src/main/java/com/taskguard/taskguard/domain/model/Task.java
@@ -1,4 +1,63 @@
 package com.taskguard.taskguard.domain.model;
 
+
+import com.taskguard.taskguard.domain.model.enums.Priority;
+import com.taskguard.taskguard.domain.model.enums.Status;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Data
+@Table(name = "tasks")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Task {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(nullable = false, name = "title")
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String title;
+
+    @Column(nullable = false, name = "description")
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String Description;
+
+    @Column(nullable = false, name = "priority")
+    @NotBlank
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+
+    @Column(nullable = false, name = "status")
+    @NotBlank
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Column(nullable = false, name = "duedate")
+    @NotBlank
+    private LocalDate dueDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, name = "createdat")
+    @NotBlank
+    private LocalDate createdAt;
+
+    @Column(nullable = false, name = "updatedat")
+    @NotBlank
+    private LocalDate updatedAt;
+
 }
